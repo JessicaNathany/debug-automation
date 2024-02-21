e25d4e1ac4f90a22362fc4ec4ea17fb198bc046a
Jéssica <jessicanathany.web@gmail.com>
2024-02-21 09:19:34 -0300

    project automation

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # debug-automation
+
+
+# Setup database
+To database configure in local environment, following this steps? 
+- Before running the project, ensure that the database setup script is executable. Run the following command:
+
+```bash
+chmod +x scripts/setup-database.sh
+
+```
+- Execute the script to configure database ./scripts/setup-database.sh
---
+++ scripts/setup-database.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if the MySQL container is running
+if [ $(docker-compose ps -q mysql | wc -l) -eq 0 ]; then
+  echo "Starting MySQL container..."
+  docker-compose up -d mysql
+fi
+
+# Check if the database exists and create it if not
+DB_EXISTS=$(docker-compose exec -T mysql mysql -uroot -proot -e "SHOW DATABASES LIKE 'cafedebug-mysql-local';")
+if [ -z "$DB_EXISTS" ]; then
+  echo "Database does not exist. Creating database..."
+  docker-compose exec -T mysql mysql -uroot -proot -e "CREATE DATABASE cafedebug-mysql-local;"
+fi
+
+# Run the SQL script to populate the database
+echo "Initializing database with data..."
+docker-compose exec -T mysql mysql -uroot -proot cafedebug-mysql < ./init/cafedebug-mysql.sql
